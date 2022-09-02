d8edc7785e37b05da70cfb5ebdad4735c702691e
Paul Ollivier <p0l@me.com>
2022-09-02 11:40:39 +0200

    Initial Commit

---
+++ Fluid Engine/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fluid Engine
+//
+//  Created by Paul Ollivier on 02/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fluid Engine/Fluid_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_EngineApp.swift
+//  Fluid Engine
+//
+//  Created by Paul Ollivier on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
